eec3a0a0f75372c17782f2163d2b8332095f1efb
JellyKuma <31178097+JellyKuma@users.noreply.github.com>
2017-08-29 21:21:53 +0800

    ver0.3

---
+++ js/data.js
@@ -0,0 +1,129 @@
+/*
+ * 设置产品的数据
+ * @param cover:封面图,front:产品正面图,back:产品背面图,title:产品标题,desc:产品描述
+ * @param imgRoute:图片相对路径，imgData:配置图片的数据
+ */
+var imgRoute = "img/clothes/";
+var imgData = [
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    },
+    {
+        cover: imgRoute+"data1-1.jpg",
+        front: imgRoute+"data1-2.jpg",
+        back: imgRoute+"data1-3.jpg",
+        title:"牛仔裤",
+        subTitle1:"高级品质",
+        subTitle2:"english",
+        desc:"商品描述",
+        new1:"new start",
+        new2:"秋季新品"
+    }
+]
